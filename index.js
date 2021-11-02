c0a786a05ad123fdfc6823a809119af5744f4489
Alan Ren <10086000+alanvww@users.noreply.github.com>
2021-11-02 08:59:52 -0400

    Trying setup Firebase

--- index.js
+++ index.js
@@ -1,15 +1,31 @@
 const express = require('express');
+// Initiate express
 const app = express();
+// Setting port - dynamically with Heroku
 const port = process.env.PORT || 3000;
+// Import Firebase
+const firebase = require('firebase/app');
+// Get configuration object so we can communicate with firebase
+const firebaseConfig = {
+	apiKey: 'AIzaSyCjjVdu-0HWrSKskB5orS36ERnkqW7syuk',
+	authDomain: 'exercise-4-fall-2021.firebaseapp.com',
+	projectId: 'exercise-4-fall-2021',
+	storageBucket: 'exercise-4-fall-2021.appspot.com',
+	messagingSenderId: '683590652249',
+	appId: '1:683590652249:web:d447344040203ff53ebc48',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+// routes for directing user to correct place
 const indexRoute = require('./routes/index');
 const articleRoute = require('./routes/article');
 const createArticleRoute = require('./routes/createArticle');
-
+// tell express to use routes
 app.use('/', indexRoute);
 app.use('/article', articleRoute);
 app.use('/create', createArticleRoute);
-
+// Listen for port
 app.listen(port, () => {
 	console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
 var express = require('express');
+// Middleware to allow for routing on the node server
 var router = express.Router();
+// require firebase
+const firebase = require('firebase/firestore/lite');
+// Initialized Firestore database
+const db = firebase.getFirestore();
+// Reference to the blogposts
+const blogposts = db.collection('blogposts');
 
 router.get('/', function (req, res) {
-	res.send('<h1>All Article</h1>');
+	// create empty array
+	const blogpostsArray = [];
+	// Get blogposts JSON from Firebase...
+	// Push document from blogposts into the blogposts array
+	res.send(blogpostsArray);
 });
 
 module.exports = router;
