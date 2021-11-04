35ad0b4d34946473177d5f7fbd303396f842f305
Alan Ren <10086000+alanvww@users.noreply.github.com>
2021-11-04 09:19:18 -0400

    All posts & Single Article

--- routes/article.js
+++ routes/article.js
@@ -1,8 +1,28 @@
+const { response } = require('express');
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function (req, res) {
-	res.send('<h1>Individual Post</h1>');
+const firestore = require('firebase/firestore');
+
+const db = firestore.getFirestore();
+
+// Get article by ID
+router.get('/:id', function (req, res) {
+	const postId = req.params.id;
+	const blogposts = firestore.getDoc(firestore.doc(db, 'blogposts', postId));
+	blogposts
+		.then((response) => {
+			const post = response.data();
+			if (post) return res.send(response.data());
+			return res.send(`NO A DOC`);
+		})
+		.catch((error) => {
+			res.send(`No Doc???`);
+		});
+});
+
+router.get('/', (req, res) => {
+	res.send(`Please include an ID`);
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,29 @@
+const { response } = require('express');
 var express = require('express');
 // Middleware to allow for routing on the node server
 var router = express.Router();
-// require firebase
-const firebase = require('firebase/firestore/lite');
+// require firestore
+const firestore = require('firebase/firestore');
 // Initialized Firestore database
-const db = firebase.getFirestore();
+const db = firestore.getFirestore();
 // Reference to the blogposts
-const blogposts = db.collection('blogposts');
-
-router.get('/', function (req, res) {
-	// create empty array
+router.get('/', (req, res) => {
+	const blogposts = firestore.getDocs(firestore.collection(db, 'blogposts'));
+	// Create empty array
 	const blogpostsArray = [];
-	// Get blogposts JSON from Firebase...
-	// Push document from blogposts into the blogposts array
-	res.send(blogpostsArray);
+
+	blogposts
+		.then((response) => {
+			response.forEach((doc) => {
+				// Push document into array every time the query loops over data and push into array
+				blogpostsArray.push(doc.data());
+			});
+			return res.send(blogpostsArray);
+		})
+		.catch(function (error) {
+			console.log('Error', error);
+			return res.send(error);
+		});
 });
 
 module.exports = router;
